d1772d741b781bf965cdbed42dc1262e7c775b75
David Cramer <dcramer@gmail.com>
2010-12-28 17:30:32 -0800

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class mytest(test):
 
 setup(
     name='nexus-memcache',
-    version='0.2.0',
+    version='0.3.0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/nexus-memcache',
